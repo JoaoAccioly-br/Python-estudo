75d6c18164079f1f00c538c56474a96383291d02
João_Accioly <jguilhermeaccioly@gmail.com>
2025-10-10 21:51:12 -0300

    Create variaveis.py

---
+++ Básico/variaveis.py
@@ -0,0 +1,24 @@
+# Declaração de variáveis
+# nome = valor
+'''
+nome = 'paulo' #String
+numero = 123456789 #Int
+existe = True #Bool
+altura = 1.75 #Float
+'''
+# Váriaveis locais e globais
+
+Media = 0 #Variável global
+total = 0 #Variável global
+
+def media_nota():
+    global Media, total #Para acessar uma variável global é só colocar (global variável)
+    for i in range(4):
+        nota = int(input('Insira a nota nos 4 bimestres: ')) #Variável local
+        total += nota # Para realizar essa soma por meio de somente variáveis, precisa ser declarado antes
+        Media = total / 4
+    print(Media) # Retorna str
+
+media_nota()
+
+
