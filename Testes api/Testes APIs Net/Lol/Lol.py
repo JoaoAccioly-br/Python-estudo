0b76fc8539d56a9647dc27e0a1a216f8ab7c9652
João_Accioly <jguilhermeaccioly@gmail.com>
2025-10-21 13:35:18 -0300

    Avanço nos estudos sobre APIs

---
+++ Testes api/Testes APIs Net/Lol/Lol.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+champion = input('Insert a character name:')
+url = 'https://ddragon.leagueoflegends.com/cdn/15.20.1/data/pt_BR/champion/{champion}.json'
+
+response = requests.get(url.format(champion=champion))
+if response.status_code == 200:
+    data = response.json()
+elif response.status_code >= 400:
+    print('Character not found')
+    pass
+
+with open(f'Testes api/Testes APIs Net/Lol/{champion}.json', 'w', encoding='utf-8') as create_file:
+    json.dump(data,create_file, ensure_ascii='utc-8', indent=4)
+
+with open(f'Testes api/Testes APIs Net/Lol/{champion}.json', 'r', encoding='utf-8') as open_file:
+    content = json.load(open_file)
+    print(content['data'][champion].keys())
