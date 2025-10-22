eac1d8a1a660fece97460ce0192f8b52b4cf88fd
João_Accioly <jguilhermeaccioly@gmail.com>
2025-10-22 13:08:16 -0300

    Update Lol.py

--- Testes api/Testes APIs Net/Lol/Lol.py
+++ Testes api/Testes APIs Net/Lol/Lol.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-champion = input('Insert a character name:')
+champion = input('Insert a character name:').capitalize()
 url = 'https://ddragon.leagueoflegends.com/cdn/15.20.1/data/pt_BR/champion/{champion}.json'
 
 response = requests.get(url.format(champion=champion))
@@ -9,7 +9,6 @@ if response.status_code == 200:
     data = response.json()
 elif response.status_code >= 400:
     print('Character not found')
-    pass
 
 with open(f'Testes api/Testes APIs Net/Lol/{champion}.json', 'w', encoding='utf-8') as create_file:
     json.dump(data,create_file, ensure_ascii='utc-8', indent=4)
